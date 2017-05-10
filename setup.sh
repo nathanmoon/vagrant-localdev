b6d84dc3ca7dd15f931eb6b92d01248c46bc2f13
Nathan Moon <nathannospam@gmail.com>
2017-05-10 09:15:12 -0600

    initial Vagrantfile

--- setup.sh
+++ setup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install vim
+sudo apt-get install -y git build-essential
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get autoremove -y
+
+cp bashrc ~/.bashrc
 
 cp gitconfig ~/.gitconfig
 
-sudo update-alternatives --set editor /usr/bin/vim.basic
+#sudo update-alternatives --set editor /usr/bin/vim.basic
 
 #echo -n "For git"
 #echo -n "Enter your name: "
@@ -23,6 +28,9 @@ EOF
 
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
-cp vimrc ~/.vimrc
+#cp vimrc ~/.vimrc
 
-vim +PluginInstall +qall
+#vim +PluginInstall +qall
+
+#cp -R tmuxp ~/.tmuxp
+
